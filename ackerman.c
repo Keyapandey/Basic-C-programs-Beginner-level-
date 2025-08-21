2f0ebd01817550261adf260956a53e25278dc3cb
Keyapandey <keyapandey98@gmail.com>
2025-08-21 16:53:04 +0530

    Add files via upload

---
+++ ackerman.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+ int ackerman(int m,int n)
+ {
+ if(m==0)
+  return n+1;
+ else if(n==0)
+  return ackerman(m-1,1);
+ else 
+  return ackerman(m-1,ackerman(m,n-1));
+}
+int main(){
+    int m,n;
+    printf("\nEnter the values of m and n\n");
+    scanf("%d %d",&m,&n);
+    printf("\nAckerman(%d %d)=%d\n",m,n,ackerman(m,n));
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
